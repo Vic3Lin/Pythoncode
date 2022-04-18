cbbb4dcb93d8dc80f8fd7b27c07f5475086e3170
Vic3Lin <103929868+Vic3Lin@users.noreply.github.com>
2022-04-19 01:43:20 +0800

    Add files via upload

---
+++ PotholeFilling.py
@@ -0,0 +1,94 @@
+"""
+File: PotholeFilling.py
+Name: TODO:
+--------------------------
+This program shows karel filling 3
+potholes. Students learn the concept of
+decomposition through the process.
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    for i in range(3):
+        go_in()
+        put_beeper99()
+        go_out()
+    turn_left()
+    turn_left()
+    for i in range(3):
+        go_iin()
+        pick_beeper99()
+        go_oout()
+
+
+def go_iin():
+    move()
+    turn_left()
+    move()
+
+
+def go_oout():
+    turn_right()
+    turn_right()
+    move()
+    turn_left()
+    move()
+
+
+def pick_beeper99():
+    for i in range(99):
+        pick_beeper()
+
+def go_in():
+    """
+    pre-condition:Karel is at the upper left of the pothole,facing East
+    post-condition:Karel is in the pothole,facing South
+    """
+    move()
+    turn_right()
+    move()
+
+
+def turn_right():
+    for i in range(3):
+        turn_left()
+
+
+def put_beeper99():
+    for i in range(99):
+        put_beeper()
+
+
+def go_out():
+    """
+    pre-condition:Karel is in the pothole,facing South
+    post-condition:Karel is at the upper left of the pothole,facing East
+    """
+    for i in range(2):
+        turn_left()
+    move()
+    turn_right()
+    move()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+####### DO NOT EDIT CODE BELOW THIS LINE ########
+
+if __name__ == '__main__':
+    execute_karel_task(main)
